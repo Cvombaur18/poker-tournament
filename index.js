d610141042d1992b24e06130fd1316e0601c1e89
Chris Vombaur <cvombaur@gmail.com>
2021-10-20 21:29:29 -0600

    deploy

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, "../index.html"));
+})
+
+const port = process.env.PORT || 4005;
+
+app.listen(port, () => {
+    console.log(`Running on port ${port}`);
+})
+
 var button = document.querySelector('.increase-btn');
 var counter = document.querySelector('.counter');
 
